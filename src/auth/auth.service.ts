339d87526f2841cc150ef48084d4a1d698622912
Md. Salahuddin <salahuddin@sharetrip.net>
2022-12-04 15:45:46 +0600

    signup with password hash by argon

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
+import { AuthDto } from './dto';
+import * as argon from 'argon2';
 
 @Injectable()
 export class AuthService {
@@ -8,7 +10,15 @@ export class AuthService {
     return { message: 'chesta' };
   }
 
-  signup() {
-    return { message: 'monobol' };
+  async signup(dto: AuthDto) {
+    const hash = await argon.hash(dto.password);
+    const user = await this.prisma.user.create({
+      data: {
+        email: dto.email,
+        hash: hash,
+      },
+    });
+
+    return user;
   }
 }
